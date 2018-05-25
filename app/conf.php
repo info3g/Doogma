f87f0ac08278e8071dc8861c74a482bf4a53a0b0
info3g <infothreeg@gmail.com>
2018-05-25 12:47:23 +0530

    Update conf.php

--- app/conf.php
+++ app/conf.php
@@ -2,5 +2,5 @@
 	define('BC_AUTH_SERVICE', 'https://login.bigcommerce.com');
 	define('BC_CLIENT_ID', 'gq98apmftghdptreh2utos3hs4w0sp0');
 	define('BC_CLIENT_SECRET', 'pmgi6dukoqh08hh72kurug9donfo8my');
-	define('BC_CALLBACK_URL', 'https://bc.doogma.com/app/oauth.php');
+	define('BC_CALLBACK_URL', 'https://doogma-app.herokuapp.com/app/oauth.php');
 ?>
